dcca1a775aaa1e5b7e65dc74acdcbd1f63c93400
alicebrasquies <lilou.brasquies@gmail.com>
2016-06-30 14:42:32 +0200

    Update passoff.php

--- passoff.php
+++ passoff.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<meta charset="utf-8"/>
-	<TITLE>Donkili-Do</TITLE>
+	<TITLE>Donkili Do</TITLE>
 	<link rel="stylesheet" href="form.css"/>
 	<link href="LOGO_accueil2.png" rel="shortcut icon" >
 </head>
@@ -9,7 +9,20 @@
 <body>
 <div class="header">
 	<a href="accueil.html"><div class="photo"></div></a>
-   	<center><H1> CHORALE DE PUGET-SUR-DURANCE</H1></center>
+	
+	<div class="session">
+		<script src="script_1.js"></script>
+		<form name=login>
+			<table class="passlog">
+				<tr><td class="titre_p">Identifiant</td><td><input type=text name=username style="height:20px; width:100px;"></td></tr>
+				<tr><td class="titre_p">Mot de passe</td><td><input type=password name=password style="height:20px; width:100px;"></td></tr>
+				<tr><td class="titre_p"><input type=button value="Connexion" onClick="Login()"></td></tr><br>
+			</table>
+			<a href="contact.html">Mot de passe oublié ?</a>
+		</form> 
+	</div>
+	
+   	<center><H1> DONKILI DO <BR> CHORALE DE PUGET-SUR-DURANCE</H1></center>
 </div>
 
 <div align=center>
